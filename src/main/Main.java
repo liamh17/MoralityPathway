f3153a2b0e372826a8718f2772f29a70e08803a8
Liam Heisler <Liam Heisler>
2016-02-29 00:34:17 -0500

    Update pauses

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,29 +8,25 @@ public class Main {
 
 	static Scanner sc = new Scanner(System.in);
 	static int dignityVariable = 0; 
-	static int lowDignity = -2; 
-	static int highDignity = 2; 
+	static int lowDignity = -3; 
+	static int highDignity = 3; 
 	static Timer timer = new Timer(); 
 		
 	public static void main(String[] args) throws InterruptedException {
-
 		String initialChoice; 
 		
-		System.out.println("Welcome to the Morality Pathway Adventure!");
-		System.out.println("--------------------------------");
+		System.out.println("<> Welcome to the Morality guidance application!");
 		Thread.sleep(2000);
-		
-		System.out.println("Here you'll be able to find a pathway"); 
-	    System.out.println("that will lead you to the most moral life possible!");
-		System.out.println("--------------------------------");
+		System.out.println("<> Here you will be able to discover the dignity you possess");
+		System.out.println("and the morality you demonstrate to others..");
 		Thread.sleep(2000);
-		
-		System.out.println("There are two choices, yes and no. Enter these"); 
+		System.out.println("<> You'll be asked many questions and at the end, based");
+		System.out.println("on your answers, given advice or reassurance.");
+		Thread.sleep(2000);
+		System.out.println("<> There are two answer choices, yes and no. Enter these"); 
 	    System.out.println("based on the question you are asked.");
-		System.out.println("--------------------------------");
-		Thread.sleep(2000);
-		
-		System.out.println("ONLY yes or no SHOULD BE TYPED. MUST BE TYPED EXACTLY AS SHOWN.");
+	    Thread.sleep(2000);
+	    System.out.println("<> Have fun and goodluck!");
 		System.out.println("--------------------------------");
 		Thread.sleep(2000);
 		
@@ -39,48 +35,45 @@ public class Main {
 		
 		if(initialChoice.equals("yes") || initialChoice.contains("y")) {
 			System.out.println("--------------------------------");
-			System.out.println("Cool! Let's begin." );
+			System.out.println("<> Cool! Let's begin." );
+			Thread.sleep(2000);
+			System.out.println("<> There are many ways to respect the dignity of others, by being curteous,"); 
+			System.out.println("kind, and respectful to those around you. ");
+			Thread.sleep(2000); 
+			System.out.println("<> You will now be asked a series of questions, please respond honestly...");
 			System.out.println("--------------------------------");
 			Thread.sleep(2000);
 			
-			System.out.println("There are many ways to respect the dignity of others, by being curteous,"); 
-			System.out.println("kind, and respectful to those around you. ");
-			System.out.println("--------------------------------");
-			Thread.sleep(2000); 
-			
-			System.out.println("You will now be asked a series of questions, please respond honestly..");
-			System.out.println("--------------------------------");
-			Thread.sleep(2000); 
-			
-			church(); 
-			Thread.sleep(600);
-			
-			responsibilityCurteous();
-			Thread.sleep(600);
-			
-			kind(); 
-			Thread.sleep(600);
-			
-            moralityOpinionQuestion();
-            Thread.sleep(1000); 
+			try {
+				church(); 
+				Thread.sleep(600);
+			}
+			catch(InterruptedException ex) {
+				System.out.println(ex.getMessage());
+			}
 		}
 		else if(initialChoice.equals("no") || initialChoice.contains("n")) {
-			System.out.println("--------------------------------");
-			System.out.println("Aww, too bad. Goodbye!");
-			System.out.println("--------------------------------");
+			System.out.println("<> Aww, too bad. Goodbye!");
 			Thread.sleep(2000);
 			
-			System.out.println("I hope you'll be able to find your own way in life and");
+			System.out.println("<> I hope you'll be able to find your own way in life and");
 			System.out.println("live as Jesus wants you to, respecting human dignity and");
 			System.out.println("encouraging others to do the same.");
-			System.out.println("--------------------------------");
+			System.out.println("----------------The End----------------");
 			
 			System.exit(0);
+			
+			try {
+				church(); 
+				Thread.sleep(600);
+			}
+			catch(InterruptedException ex) {
+				System.out.println(ex.getMessage());
+			}
 		}
 	}
 	
 	public static void church() throws InterruptedException{
-		
 		String churchChoice; 
 		
 		System.out.println("-> Do you go to church?");
@@ -88,6 +81,13 @@ public class Main {
 		
 		if(churchChoice.equals("yes") || churchChoice.contains("y")) {
 			dignityVariable++; 
+			try {
+				responsibilityCurteous(); 
+				Thread.sleep(600);
+			}
+			catch(InterruptedException ex) {
+				System.out.println(ex.getMessage());
+			}
 		}
 		else if(churchChoice.equals("no") || churchChoice.contains("n")) {
 			dignityVariable--;
@@ -101,7 +101,6 @@ public class Main {
 		}
 	}
 	public static void responsibilityCurteous() throws InterruptedException {
-		
 		String responCurtChoice;
 		
 		System.out.println("-> Have you been respectul and curteous to other people?");
@@ -109,9 +108,60 @@ public class Main {
 		
 		if(responCurtChoice.equals("yes") || responCurtChoice.contains("y")) {
 			dignityVariable++; 
+			try {
+				fundGood(); 
+				Thread.sleep(600);
+			}
+			catch(InterruptedException ex) {
+				System.out.println(ex.getMessage());
+			}
 		}
 		else if(responCurtChoice.equals("no") || responCurtChoice.contains("n")) {
 			dignityVariable--; 
+			try {
+				fundGood(); 
+				Thread.sleep(600);
+			}
+			catch(InterruptedException ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+	}
+	public static void fundGood() throws InterruptedException {
+		String fundGoodChoice;
+		
+		System.out.println("-> Do you believe humans are fundamentally good?");
+		fundGoodChoice = sc.nextLine(); 
+		
+		if(fundGoodChoice.equals("yes") || fundGoodChoice.contains("y")) {
+			dignityVariable++; 
+			try {
+				trueToSelfChoice(); 
+				Thread.sleep(600);
+			}
+			catch(InterruptedException ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+		else if(fundGoodChoice.equals("no") || fundGoodChoice.contains("n")) {
+			dignityVariable--; 
+			try {
+				trueToSelfChoice(); 
+				Thread.sleep(600);
+			}
+			catch(InterruptedException ex) {
+				System.out.println(ex.getMessage());
+			}
+		}
+	}
+	public static void trueToSelfChoice() throws InterruptedException {
+		String trueToSelfChoice; 
+		
+		System.out.println("-> Have you been true to yourself?");
+		trueToSelfChoice = sc.nextLine(); 
+		
+		if(trueToSelfChoice.equals("yes") || trueToSelfChoice.contains("y")) {
+			dignityVariable++; 
 			try {
 				kind(); 
 				Thread.sleep(600);
@@ -120,9 +170,20 @@ public class Main {
 				System.out.println(ex.getMessage());
 			}
 		}
+		else if(trueToSelfChoice.equals("yes") || trueToSelfChoice.contains("n")) {
+			dignityVariable--; 
+			try {
+				kind();
+				Thread.sleep(600); 
+			}
+			catch(InterruptedException ex) {
+				kind(); 
+				Thread.sleep(600);
+			}
+			
+		}
 	}
 	public static void kind () throws InterruptedException {
-		
 		String kindChoice;
 		
 		System.out.println("-> Have you been kind to those around you?");
@@ -144,7 +205,6 @@ public class Main {
 		}
 	}
 	public static void moralityOpinionQuestion() throws InterruptedException {
-		
 		String finalQuestion;
 		
 		System.out.println("-> Do you think you've lived a moral life?");
@@ -152,45 +212,42 @@ public class Main {
 		
 		if(finalQuestion.contains("y") && dignityVariable >= highDignity) {
 			System.out.println("--------------------------------");
-			System.out.println("Based on your responses to these questions, you are correct. You have respected");
+			System.out.println("<> Based on your responses to these questions, you are correct. You have respected");
 			System.out.println("the dignity of others and have set wonderful examples for those around us.");
-			System.out.println("--------------------------------");
 			Thread.sleep(2000);
-			
-			System.out.println("You've done great. You are on the right track to Heaven and have been");
+			System.out.println("<> You've done great. You are on the right track to Heaven and have been");
 			System.out.println("the best version of yourself.");
-			System.out.println("--------------------------------");
+			System.out.println("----------------The End----------------");
 			
 			System.exit(0); 
 		}
 		else if(finalQuestion.contains("y") && dignityVariable >= lowDignity) {
 			System.out.println("--------------------------------");
-			System.out.println("You've done good but you could use some work in the area's you've responded no to.");
-			System.out.println("There is nothing you cannot do when you put your mind to it, the same");
+			System.out.println("<> You've done good but you could use some work in the area's you've responded no to.");
+			Thread.sleep(3000);
+			System.out.println("<> There is nothing you cannot do when you put your mind to it, the same");
 			System.out.println("principle applies here. If you work towards making yourself the best version");
 			System.out.println("of yourself by treating people correctly, fairly and respectfully, you can");
 			System.out.println("reach Heaven with ease.");
-			System.out.println("--------------------------------");
 			Thread.sleep(3000);
-			
-			System.out.println("I hope these questions have helped you learn about what you need to fix in the");
+			System.out.println("<> I hope these questions have helped you learn about what you need to fix in the");
 			System.out.println("way you treat people, how you look at them, and how to fix it.");
-			System.out.println("--------------------------------");
+			System.out.println("----------------The End----------------");
 			
 			System.exit(0); 
 		}
 		else if(finalQuestion.contains("n")) {
 			System.out.println("--------------------------------");
-			System.out.println("Based on these responses to your questions, you are lying to yourself.");
-			System.out.println("You've proven that you're a wonderful person and that you have lived a moral,");
-			System.out.println("respectful and wonderful life so far and I hope you can continue to do so.");
-			System.out.println("--------------------------------");
-			Thread.sleep(3000);
-			
-			System.out.println("You should work to help yourself realize such a thing as this, and if you do,");
-			System.out.println("you might just inspire yourself to continue to do the good work you've");
-			System.out.println("been doing.");
-			System.out.println("--------------------------------");
+			System.out.println("<> Based on your responses to these questions, you could use some work.");
+			Thread.sleep(1000);
+			System.out.println("<> You've done some things, both good and bad. Respecting others, being kind, ");
+			System.out.println("and being the best person you can be are the best ways to live a moral life.");
+			Thread.sleep(2000);
+			System.out.println("<> Doing these things is the best way to not only increase your own dignity, but");
+			System.out.println("increase the dignity of those around you. You are one of 7.1 billion people on Earth");
+			System.out.println("and the only way you can make a difference is by being the difference you want to ");
+			System.out.println("see in the world.");
+			System.out.println("----------------The End----------------");
 			
 			System.exit(0);
 		}
